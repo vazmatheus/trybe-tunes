4ad4dd917ffa02e6bd93ce5af27817fa3f1c8edc
Matheus Vaz <matheusvaz@matheuss-air.lan>
2022-01-14 14:16:40 -0800

    requirement 1

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Notfound extends Component {
+  render() {
+    return (
+      <div data-testid="page-not-found" />
+    );
+  }
+}
+
+export default Notfound;
